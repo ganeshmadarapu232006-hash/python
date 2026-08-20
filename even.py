4856bd8651b9304f0929c9cbfc1807592736df13
Ganga Manohar <ganeshmadarapu232006@gmail.com>
2026-08-20 10:23:56 +0530

    add mod2

---
+++ even.py
@@ -0,0 +1,5 @@
+a = 4
+if a%2==0:
+    print("Given number is even")
+else:
+    print("Given number is odd")
---
+++ example.py
@@ -0,0 +1,5 @@
+a = 4
+if a<=2:
+    print("Given number is smaller")
+else:
+    print("Given number is larger")
